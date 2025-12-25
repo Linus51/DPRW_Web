0d9956c74db919a21d2d0a3c957fc7c140402081
Linus51 <linusknoch6@gmail.com>
2025-12-25 02:05:03 +0100

    Update discord.ts

--- lib/discord.ts
+++ lib/discord.ts
@@ -1,5 +1,5 @@
 export async function sendDiscordWebhook(content: string, embeds?: any[]) {
-  const url = process.env.https://discordapp.com/api/webhooks/1434321349823828168/SEXlc0Y1Hey06Q1ulJGN9ti99kDL-Rq_k3eKshG527YMuSeo8Xke6hdfnPKbSWSMvd5C;
+  const url = process.env.discordapp.com/api/webhooks/1434321349823828168/SEXlc0Y1Hey06Q1ulJGN9ti99kDL-Rq_k3eKshG527YMuSeo8Xke6hdfnPKbSWSMvd5C;
   if (!url) return; // optional
   await fetch(url, {
     method: "POST",
